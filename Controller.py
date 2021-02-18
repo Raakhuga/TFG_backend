edefeae10ace6a8e9a0cafb7f7516a980f527239
Raakhuga <santirubiomontilla@gmail.com>
2021-02-18 16:29:29 +0100

    Can bus read implementation

--- Controller.py
+++ Controller.py
@@ -15,6 +15,10 @@ def run_server(start_server, loop):
     loop.run_until_complete(start_server)
     loop.run_forever()
 
+def read_can(can):
+    while True: 
+        can.read()
+
 # Create observable variables
 speed = ValueObserver(0)
 rpm = ValueObserver(0)
@@ -23,11 +27,20 @@ distance = ValueObserver(0)
 # Initiate thread with websockets api
 loop = asyncio.get_event_loop()
 server = API.Server(loop, speed, distance, rpm)
-start_server = websockets.serve(server.ws_handler, 'localhost', 4000)
+start_server = websockets.serve(server.ws_handler, '192.168.100.1', 4000)
 thread = threading.Thread(target=run_server, args=(start_server, loop))
 thread.start()
 
 # Initiate can bus reader
+can = Default_CAN_device('vcan0')
+can = CAN_speed(can, hex(1), speed)
+can = CAN_rpm(can, hex(2), rpm)
+can = CAN_distance(can, hex(3), distance)
+
+'''
 while(1):
     time.sleep(1)
-    speed.value += 1
+    speed.value += 10
+    if (speed.value == 200):
+        speed.value = 0
+'''
